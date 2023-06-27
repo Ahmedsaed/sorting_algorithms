afa05a96c8f8cd072b13614e192d332dffcb4da2
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-06-27 23:37:22 +0300

    update quick sort hoare logic

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,9 +1,9 @@
 #include "sort.h"
 
 /* helper functions */
-void quick_sort_helper(int* array, int low, int high, size_t size);
-int partition(int* array, int low, int high, size_t size);
-void swap(int* a, int* b);
+void quick_sort_helper(int *array, size_t size, int low, int high);
+int partition(int *array, size_t size, int low, int high);
+void swap(int *a, int *b);
 
 /**
  * quick_sort_hoare - sorts an array of integers in ascending
@@ -17,7 +17,7 @@ void quick_sort_hoare(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	quick_sort_helper(array, 0, size - 1, size);
+	quick_sort_helper(array, size, 0, size - 1);
 }
 
 /**
@@ -29,52 +29,51 @@ void quick_sort_hoare(int *array, size_t size)
  * @high: the high index
  * @size: the size of the array
  */
-void quick_sort_helper(int* array, int low, int high, size_t size)
+void quick_sort_helper(int *array, size_t size, int low, int high)
 {
 	int pivot_index;
 
-    if (low < high)
+	if (high - low > 0)
 	{
-        pivot_index = partition(array, low, high, size);
+		pivot_index = partition(array, size, low, high);
 
-        quick_sort_helper(array, low, pivot_index - 1, size);
-        quick_sort_helper(array, pivot_index + 1, high, size);
-    }
+		quick_sort_helper(array, size, low, pivot_index - 1);
+		quick_sort_helper(array, size, pivot_index, high);
+	}
 }
 
 /**
  * partition - partitions the array
  *
  * @array: the array to partition
+ * @size: the size of the array
  * @low: the low index
  * @high: the high index
+ *
+ * Return: the pivot index
 */
-int partition(int* array, int low, int high, size_t size) {
-    int pivot = array[high];
-    int i = (low - 1);
-    int j = high;
+int partition(int *array, size_t size, int low, int high)
+{
+	int pivot = array[high], i, j;
 
-    while (1)
+	for (i = low - 1, j = high + 1; i < j;)
 	{
-        do {
-            i++;
-        } while (array[i] < pivot);
+		do {
+			i++;
+		} while (array[i] < pivot);
 
-        do {
-            j--;
-        } while (array[j] > pivot && j > low);
+		do {
+			j--;
+		} while (array[j] > pivot);
 
-        if (i >= j)
-            break;
+		if (i < j)
+		{
+			swap(&array[i], &array[j]);
+			print_array(array, size);
+		}
+	}
 
-        swap(&array[i], &array[j]);
-		print_array(array, size);
-    }
-
-    swap(&array[i], &array[high]);
-    print_array(array, size);
-
-    return (i);
+	return (i);
 }
 
 /**
@@ -83,8 +82,10 @@ int partition(int* array, int low, int high, size_t size) {
  * @a: the first integer
  * @b: the second integer
 */
-void swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void swap(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
 }
