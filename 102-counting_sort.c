f40158989623f62115d4c30e26f2cb10331ef872
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-06-27 03:01:31 +0300

    code refactoring for counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,6 +1,27 @@
 #include "sort.h"
 
 /**
+ * get_max - gets the maximum value in an array
+ *
+ * @array: array of integers
+ * @size: size of the array
+ *
+ * Return: maximum value in the array
+ */
+size_t get_max(int *array, size_t size)
+{
+	int max;
+	size_t i;
+
+	max = 0;
+	for (i = 1; i < size; i++)
+		if (array[i] > array[max])
+			max = i;
+
+	return (array[max]);
+}
+
+/**
  * counting_sort - sorts an array of integers in
  * ascending order using the Counting sort algorithm
  *
@@ -15,25 +36,20 @@ void counting_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	max = 0;
-	for (i = 1; i < size; i++)
-		if (array[i] > array[max])
-			max = i;
+	max = get_max(array, size);
 
-	freq = malloc(sizeof(int) * (array[max] + 1));
+	freq = malloc(sizeof(int) * (max + 1));
 	if (freq == NULL)
 		return;
 
-	for (i = 0; (int)i < array[max] + 1; i++)
+	for (i = 0; i < max + 1; i++)
 		freq[i] = 0;
-
 	for (i = 0; i < size; i++)
 		freq[array[i]]++;
-
-	for (i = 1; (int)i < array[max] + 1; i++)
+	for (i = 1; i < max + 1; i++)
 		freq[i] += freq[i - 1];
 
-	print_array(freq, array[max] + 1);
+	print_array(freq, max + 1);
 
 	output = malloc(sizeof(int) * size);
 	if (output == NULL)
